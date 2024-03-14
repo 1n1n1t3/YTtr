38fd7f297d8724b09194964fadade45c217b6ce1
Dimitar Davidkov <mitko105@gmail.com>
2024-03-14 21:37:49 +0200

    redeploy

--- api/main.py
+++ api/main.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import youtube_transcript_api
 import datetime
+
 app = Flask(__name__)
 
 CORS(app)  # Enable CORS for all routes
