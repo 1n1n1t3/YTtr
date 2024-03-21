19884c2f73872e421378ddcc6e3a97e32a42eccf
Dimitar Davidkov <mitko105@gmail.com>
2024-03-21 17:27:39 +0200

    remove ms

--- api/main.py
+++ api/main.py
@@ -97,7 +97,7 @@ def get_video_details(video_id):
         # Fetch transcript with timestamps
         try:
             transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id)
-            transcript_text = '\n'.join([f"[{datetime.timedelta(seconds=entry['start'])}] {entry['text']}" for entry in transcript])
+            transcript_text = '\n'.join([f"[{str(datetime.timedelta(seconds=entry['start']))[:-4]}] {entry['text']}" for entry in transcript])
         except Exception as e:
             print(f"Error fetching transcript: {e}")
             transcript_text = None
