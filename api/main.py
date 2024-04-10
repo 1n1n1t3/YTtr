3a0a28036063346389c731b2075705b5d645baa4
Dimitar Davidkov <mitko105@gmail.com>
2024-04-10 15:36:08 +0300

    more languages

--- api/main.py
+++ api/main.py
@@ -123,7 +123,7 @@ def get_video_details(video_id):
 
         # Fetch transcript with timestamps
         try:
-            transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'de', 'jp', 'fr', 'pt', 'es'])
+            transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'de', 'jp', 'fr', 'pt', 'es', 'bg', 'mk','ru','ro','ch'])
             transcript_text = '\n'.join([f"{str(datetime.timedelta(seconds=int(entry['start'])))} {entry['text']}" for entry in transcript])
         except Exception as e:
             print(f"Error fetching transcript: {e}")
