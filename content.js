c0176a2743d1f28ca7ca8c7ba622406fb8014f7e
Dimitar Davidkov <mitko105@gmail.com>
2024-05-10 14:13:30 +0300

    clean

--- content.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'seekToTimestamp') {
-        var player = document.querySelector('video');
-        if (player) {
-            player.currentTime = request.timestamp;
-        }
-    }
-});
