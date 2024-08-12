176515561098859942526118df98fe733e1f3718
Dimitar Davidkov <mitko105@gmail.com>
2024-08-12 14:37:25 +0300

    free proxy

--- api/main.py
+++ api/main.py
@@ -126,7 +126,7 @@ def get_video_details(video_id):
             encoded_user = os.environ.get('NORD_USER', '')
             encoded_pass = os.environ.get('NORD_PASSWORD', '')
             # _proxies = {"https": f"https://{encoded_user}:{encoded_pass}@amsterdam.nl.socks.nordhold.net:1080"}
-            _proxies = {"https": f"194.182.178.90:3128"}
+            _proxies = {"https": f"36.72.252.220:8080"}
             transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'de', 'jp', 'fr', 'pt', 'es', 'ru','it','ko','nl'], 
                                                                                     proxies=_proxies)
             transcript_text = '\n'.join([f"{str(datetime.timedelta(seconds=int(entry['start'])))} {entry['text']}" for entry in transcript])
