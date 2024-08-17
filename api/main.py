3462d376ebe658a797f808a1b84dc5af72462129
Dimitar Davidkov <mitko105@gmail.com>
2024-08-17 19:00:12 +0300

    no proxy

--- api/main.py
+++ api/main.py
@@ -125,10 +125,10 @@ def get_video_details(video_id):
         try:
             encoded_user = os.environ.get('NORD_USER', '')
             encoded_pass = os.environ.get('NORD_PASSWORD', '')
-            # _proxies = {"https": f"https://{encoded_user}:{encoded_pass}@amsterdam.nl.socks.nordhold.net:1080"}
-            _proxies = {"https": f"36.72.252.220:8080"}
+           # _proxies = {"https": f"https://{encoded_user}:{encoded_pass}@amsterdam.nl.socks.nordhold.net:1080"}
+            # _proxies = {"https": f"36.72.252.220:8080"}
             transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'de', 'jp', 'fr', 'pt', 'es', 'ru','it','ko','nl'], 
-                                                                                    proxies=_proxies)
+                                                                                    )#proxies=_proxies)
             transcript_text = '\n'.join([f"{str(datetime.timedelta(seconds=int(entry['start'])))} {entry['text']}" for entry in transcript])
         except Exception as e:
             print(f"Error fetching transcript: {e}")
